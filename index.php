2f1ebe055381db7ecd580ba48b1bcaa7282c1722
kudangrm33 <kudangrm33@gmail.com>
2026-05-20 19:59:57 +0700

    index.php

--- index.php
+++ index.php
@@ -299,6 +299,7 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <div class="row">
         <template x-for="(item, index) in items" x-key="index">
           <div class="product-card" :class="{'unavailable': item.available <= 0}">
+
             <div class="product-icons">
               <a href="#" @click.prevent="$store.cart.add({ ...item, name: item.name || item.NAME })">
                 <svg
